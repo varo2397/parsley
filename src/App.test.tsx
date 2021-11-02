8fc8230b23504505dcdc90cc473283400537ee4e
Alvaro Castro <alvarocastro74@gmail.com>
2021-11-02 12:13:17 -0600

    test2

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders learn react link1', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
